bb000cedd4bf527cd5823455973d0ca5a8c650f8
syter <syter@arrownock.com>
2019-01-02 21:38:54 +0800

    a

---
+++ HappyF/app/src/main/java/com/sky/happyf/activity/HappyListActivity.java
@@ -0,0 +1,66 @@
+package com.sky.happyf.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.sky.happyf.R;
+
+public class HappyListActivity extends AppCompatActivity {
+    private EditText etPhone;
+    private EditText etPwd;
+    private Button btnLogin;
+    private Button btnReg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_happy_list);
+
+        initView();
+
+        initListener();
+
+
+    }
+
+    private void initView() {
+        etPhone = (EditText) findViewById(R.id.et_phone);
+        etPwd = (EditText) findViewById(R.id.et_pwd);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnReg = (Button) findViewById(R.id.btn_reg);
+    }
+
+    private void initListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(it);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.bottom_silent, R.anim.anim_exit);
+    }
+}
--- HappyF/app/src/main/java/com/sky/happyf/mainHelper/MineHelper.java
+++ HappyF/app/src/main/java/com/sky/happyf/mainHelper/MineHelper.java
@@ -1,11 +1,27 @@
 package com.sky.happyf.mainHelper;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.sky.happyf.R;
+import com.sky.happyf.activity.CartListActivity;
+import com.sky.happyf.activity.EditUserActivity;
+import com.sky.happyf.activity.HappyListActivity;
+import com.sky.happyf.activity.LoginActivity;
+import com.sky.happyf.activity.MainActivity;
+import com.sky.happyf.activity.OrderListActivity;
+import com.sky.happyf.activity.RankListActivity;
+import com.sky.happyf.activity.SetActivity;
+import com.sky.happyf.activity.VipActivity;
 
 public class MineHelper {
     private View view;
     private Activity act;
+    private RelativeLayout rlEdit, rlOrder, rlCart, rlHappy, rlRank, rlSet;
+    private ImageView ivVip;
 
     public MineHelper(Activity act, View view) {
         this.act = act;
@@ -21,10 +37,71 @@ public class MineHelper {
     }
 
     private void initView() {
+        rlEdit = (RelativeLayout) view.findViewById(R.id.rl_edit);
+        rlOrder = (RelativeLayout) view.findViewById(R.id.rl_edit);
+        rlCart = (RelativeLayout) view.findViewById(R.id.rl_edit);
+        rlHappy = (RelativeLayout) view.findViewById(R.id.rl_edit);
+        rlRank = (RelativeLayout) view.findViewById(R.id.rl_edit);
+        rlSet = (RelativeLayout) view.findViewById(R.id.rl_edit);
+        ivVip = (ImageView) view.findViewById(R.id.iv_vip);
     }
 
     private void initListener() {
+        rlEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                act.startActivity(new Intent(act, EditUserActivity.class));
+                act.overridePendingTransition(R.anim.anim_enter, R.anim.bottom_silent);
+            }
+        });
 
+        rlOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                act.startActivity(new Intent(act, OrderListActivity.class));
+                act.overridePendingTransition(R.anim.anim_enter, R.anim.bottom_silent);
+            }
+        });
+
+        rlCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                act.startActivity(new Intent(act, CartListActivity.class));
+                act.overridePendingTransition(R.anim.anim_enter, R.anim.bottom_silent);
+            }
+        });
+
+        rlHappy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                act.startActivity(new Intent(act, HappyListActivity.class));
+                act.overridePendingTransition(R.anim.anim_enter, R.anim.bottom_silent);
+            }
+        });
+
+        rlRank.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                act.startActivity(new Intent(act, RankListActivity.class));
+                act.overridePendingTransition(R.anim.anim_enter, R.anim.bottom_silent);
+            }
+        });
+
+        rlSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                act.startActivity(new Intent(act, SetActivity.class));
+                act.overridePendingTransition(R.anim.anim_enter, R.anim.bottom_silent);
+            }
+        });
+
+        ivVip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                act.startActivity(new Intent(act, VipActivity.class));
+                act.overridePendingTransition(R.anim.anim_enter, R.anim.bottom_silent);
+            }
+        });
     }
 
     private void initData() {
